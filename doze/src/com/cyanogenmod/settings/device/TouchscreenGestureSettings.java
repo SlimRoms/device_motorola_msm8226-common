CyanogenMod/android_device_motorola_msm8226-common

msm8226-common: doze: Add ambient display preference
Change-Id: Ib9432cc47705d9513bde198cbbfa5b481082e39d

doze/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
@@ -19,21 +19,37 @@ package com.cyanogenmod.settings.device;
 import com.android.internal.util.cm.ScreenType;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 
 public class TouchscreenGestureSettings extends PreferenceActivity {
 
-    private static final String CATEGORY_AMBIENT_DISPLAY = "ambient_display_key";
+    private static final String KEY_AMBIENT_DISPLAY_ENABLE = "ambient_display_enable";
+    private static final String KEY_GESTURE_POCKET = "gesture_pocket";
+    private static final String KEY_GESTURE_HAND_WAVE = "gesture_hand_wave";
+
+    private SwitchPreference mAmbientDisplayPreference;
+    private SwitchPreference mPickupPreference;
+    private SwitchPreference mHandwavePreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.gesture_panel);
-        PreferenceCategory ambientDisplay =
-            (PreferenceCategory) findPreference(CATEGORY_AMBIENT_DISPLAY);
-        ambientDisplay.setEnabled(isDozeEnabled());
+        boolean dozeEnabled = isDozeEnabled();
+        mAmbientDisplayPreference =
+            (SwitchPreference) findPreference(KEY_AMBIENT_DISPLAY_ENABLE);
+        // Read from DOZE_ENABLED secure setting
+        mAmbientDisplayPreference.setChecked(dozeEnabled);
+        mAmbientDisplayPreference.setOnPreferenceChangeListener(mAmbientDisplayPrefListener);
+        mPickupPreference =
+            (SwitchPreference) findPreference(KEY_GESTURE_POCKET);
+        mPickupPreference.setEnabled(dozeEnabled);
+        mHandwavePreference =
+            (SwitchPreference) findPreference(KEY_GESTURE_HAND_WAVE);
+        mHandwavePreference.setEnabled(dozeEnabled);
     }
 
     @Override
@@ -46,8 +62,27 @@ public class TouchscreenGestureSettings extends PreferenceActivity {
         }
     }
 
+    private boolean enableDoze(boolean enable) {
+        return Settings.Secure.putInt(getContentResolver(),
+                Settings.Secure.DOZE_ENABLED, enable ? 1 : 0);
+    }
+
     private boolean isDozeEnabled() {
         return Settings.Secure.getInt(getContentResolver(),
                 Settings.Secure.DOZE_ENABLED, 1) != 0;
     }
+
+    private Preference.OnPreferenceChangeListener mAmbientDisplayPrefListener =
+        new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            boolean enable = (boolean) newValue;
+            boolean ret = enableDoze(enable);
+            if (ret) {
+                mPickupPreference.setEnabled(enable);
+                mHandwavePreference.setEnabled(enable);
+            }
+            return ret;
+        }
+    };
 }

SlimRoms/device_motorola_msm8226-common

doze/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
@@ -19,21 +19,37 @@ package com.cyanogenmod.settings.device;
 import com.android.internal.util.cm.ScreenType;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 
 public class TouchscreenGestureSettings extends PreferenceActivity {
 
-    private static final String CATEGORY_AMBIENT_DISPLAY = "ambient_display_key";
+    private static final String KEY_AMBIENT_DISPLAY_ENABLE = "ambient_display_enable";
+    private static final String KEY_GESTURE_POCKET = "gesture_pocket";
+    private static final String KEY_GESTURE_HAND_WAVE = "gesture_hand_wave";
+
+    private SwitchPreference mAmbientDisplayPreference;
+    private SwitchPreference mPocketPreference;
+    private SwitchPreference mHandwavePreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.gesture_panel);
-        PreferenceCategory ambientDisplay =
-            (PreferenceCategory) findPreference(CATEGORY_AMBIENT_DISPLAY);
-        ambientDisplay.setEnabled(isDozeEnabled());
+        boolean dozeEnabled = isDozeEnabled();
+        mAmbientDisplayPreference =
+            (SwitchPreference) findPreference(KEY_AMBIENT_DISPLAY_ENABLE);
+        // Read from DOZE_ENABLED secure setting
+        mAmbientDisplayPreference.setChecked(dozeEnabled);
+        mAmbientDisplayPreference.setOnPreferenceChangeListener(mAmbientDisplayPrefListener);
+        mPocketPreference =
+            (SwitchPreference) findPreference(KEY_GESTURE_POCKET);
+        mPocketPreference.setEnabled(dozeEnabled);
+        mHandwavePreference =
+            (SwitchPreference) findPreference(KEY_GESTURE_HAND_WAVE);
+        mHandwavePreference.setEnabled(dozeEnabled);
     }
 
     @Override
@@ -46,8 +62,27 @@ public class TouchscreenGestureSettings extends PreferenceActivity {
         }
     }
 
+    private boolean enableDoze(boolean enable) {
+        return Settings.Secure.putInt(getContentResolver(),
+                Settings.Secure.DOZE_ENABLED, enable ? 1 : 0);
+    }
+
     private boolean isDozeEnabled() {
         return Settings.Secure.getInt(getContentResolver(),
                 Settings.Secure.DOZE_ENABLED, 1) != 0;
     }
+
+    private Preference.OnPreferenceChangeListener mAmbientDisplayPrefListener =
+        new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            boolean enable = (boolean) newValue;
+            boolean ret = enableDoze(enable);
+            if (ret) {
+                mPocketPreference.setEnabled(enable);
+                mHandwavePreference.setEnabled(enable);
+            }
+            return ret;
+        }
+    };
 }